8eb4769afa6f50d5e0670eea94e6848855ba4f05
Denis Chernyak <chernyak.ds@gmail.com>
2024-08-28 10:38:47 +0300

    amendements done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,21 +11,24 @@ import './App.css'
 export default function App() {
   const [query, setQuery] = useState('');
   const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);  
+  const [page, setPage] = useState(1);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [selectedImg, setSelectedImg] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [totalPages, setTotalPages] = useState(858);
 
-    useEffect(() => {
-    if (!query) return;
+  useEffect(() => {
+    if (!query || query === '') return;
 
     const fetchData = async () => {
-      setIsLoading(true);
       try {
+        setIsLoading(true);
+        setError(false)
         const data = await fetchImages(query, page);
         console.log(data.results);
         setImages(prevImages => [...prevImages, ...data.results]);
+        setTotalPages(data.totalPages);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -59,9 +62,10 @@ export default function App() {
     <div>
       <SearchBar onSubmit={handleSearchSubmit} />
       {error && <ErrorMessage message={error} />}
-      <ImageGallery images={images} onImageClick={handleImageClick} />
+      {images.length > 0 && <ImageGallery images={images} onImageClick={handleImageClick} />}
       {isLoading && <Loader />}
       {images.length > 0 && !isLoading && <LoadMoreBtn onLoadMore={() => setPage(prevPage => prevPage + 1)} />}
+      {page >= totalPages && <p> End of Collection</p>}
       <ImageModal isOpen={isModalOpen} onClose={closeModal} image={selectedImg} />
     </div>
   );
